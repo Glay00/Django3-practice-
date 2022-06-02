6504d6d580098260203fb1ec3e399542577535bb
Gleb <glebgorelov0110@mail.ru>
2022-06-02 16:58:26 +0700

    changed the docker file and added forms with html

--- firstApp/models.py
+++ firstApp/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
@@ -8,5 +9,20 @@ class Bb(models.Model):
     content=models.TextField(null=True, blank=True)
     price=models.FloatField(null=True, blank=True)
     published=models.DateTimeField(auto_now_add=True,db_index=True)
+    rubric = models.ForeignKey('Rubric',null=True,on_delete=models.PROTECT,verbose_name='Рубрика')
 
+class Meta:
+    verbose_name_plural="Объявления"
+    verbose_name="Объявление"
+    ordering=["-published"]
+
+
+class Rubric(models.Model):
+    name=models.CharField(max_length=50,db_index=True,verbose_name="Название")
+    def __str__(self):
+        return self.name
+class Meta:
+    verbose_name_plural='Рубрики'
+    verbose_name='Рубрика'
+    ordering=['name']
 
--- firstApp/views.py
+++ firstApp/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
 
-
-
+from  django.views.generic.edit import CreateView
 # from django.http import HttpResponse
 # from django.template import loader
-
-from .models import Bb
+from .forms import BbForm
+from .models import Bb, Rubric
 
 def index(request):
     # # return HttpResponse("Здесь будет текст...")
     # template=loader.get_template('firstApp/termplates/index.html')
-    bbs=Bb.objects.order_by('-published')
+    bbs=Bb.objects.all()
     context= {'bbs': bbs}
     # return HttpResponse(template.render(context,request))
 # from django.shortcuts import render
@@ -18,3 +17,16 @@ def index(request):
 # def index(request):
 #     return render(request, 'firstApp/index.html')
     return render(request,'index.html',context)
+
+def by_rubric(request,rubric_id):
+    bbs=Bb.objects.filter(rubric=rubric_id)
+    rubrics=Rubric.objects.all()
+    current_rubric=Rubric.objects.get(pk=rubric_id)
+    context={'bbs':bbs, 'rubrics':rubrics,
+            'current_rubric':current_rubric}
+    return render(request,'by_rubric.html',context) #firstApp/by_rubric.html
+    
+class BbCreateView(CreateView):
+    template_name: 'firstApp/create.html'
+    form_class=BbForm
+    success_url='firstApp'
